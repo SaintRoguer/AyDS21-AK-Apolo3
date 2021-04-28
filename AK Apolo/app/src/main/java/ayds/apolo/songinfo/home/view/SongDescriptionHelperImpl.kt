f5fcf7d44fd66a0fd468d4377cf20b9457aab98c
SaintRoguer <anonymousvsworld@gmail.com>
2021-04-28 20:16:26 -0300

    Update SongDescriptionHelperImpl.kt
    Agregadas funciones para formatear la fecha de publicacion de una cancion, tiene errores la funcion dia(), no formatea como deberia, con la cancion mocking bird, releaseDate=2019-05-15, deberia mostrar luego del formateo 15/05/2019, pero muestra 2019-05-15, o sea, sin cambios

--- AK Apolo/app/src/main/java/ayds/apolo/songinfo/home/view/SongDescriptionHelperImpl.kt
+++ AK Apolo/app/src/main/java/ayds/apolo/songinfo/home/view/SongDescriptionHelperImpl.kt
@@ -1,14 +1,18 @@
 package ayds.apolo.songinfo.home.view
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import ayds.apolo.songinfo.home.model.entities.EmptySong
 import ayds.apolo.songinfo.home.model.entities.Song
 import ayds.apolo.songinfo.home.model.entities.SpotifySong
+import java.time.format.DateTimeFormatter
 
 interface SongDescriptionHelper {
     fun getSongDescriptionText(song: Song = EmptySong): String
 }
 
 internal class SongDescriptionHelperImpl : SongDescriptionHelper {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun getSongDescriptionText(song: Song): String {
         return when (song) {
             is SpotifySong ->
@@ -18,8 +22,52 @@ internal class SongDescriptionHelperImpl : SongDescriptionHelper {
                 } \n" +
                         "Artist: ${song.artistName}  \n" +
                         "Album: ${song.albumName}  \n" +
-                        "Release Date: ${song.releaseDateModified} "
+                        "Release Date: ${parsingDateWithPrecision(song.releaseDatePrecision,song.releaseDate)}\n "
+
             else -> "Song not found"
         }
     }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun parsingDateWithPrecision(precision:String, releaseDate:String)=when{
+                                            (precision)=="day"->day(releaseDate)
+                                            (precision)=="month"->month(releaseDate)
+                                            (precision)=="year"->year(releaseDate)
+                                            else ->"An error has occurred when modifying the format of the release date"
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun day(releaseDate :String): String {
+        val formatter = DateTimeFormatter.ofPattern("dd/MM/uuuu")
+        return releaseDate.format(formatter)
+
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun month(releaseDate :String): String {
+        val formatter = DateTimeFormatter.ofPattern("MMMM-uuuu")
+        return releaseDate.format(formatter)
+
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun year(releaseDate :String): String {
+        val formatter = DateTimeFormatter.ofPattern("uuuu")
+        val year = releaseDate.format(formatter)
+        val leap = isLeapYear(year.toInt())
+        return if (!leap) "$year (not a leap year)" else year
+    }
+
+    private fun isLeapYear(year:Int):Boolean=when {
+                                                year % 4 == 0 -> {
+                                                    when {
+                                                        year % 100 == 0 -> year % 400 == 0
+                                                        else -> true
+                                                    }
+                                                }
+                                                else -> false
+    }
+
+
+
+
 }
